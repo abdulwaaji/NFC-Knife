b6a8c089112c8ad940fb7e79d45856a4bf130b6a
abdulwaajid <abdu.waajid@gmail.com>
2024-05-27 07:09:13 +0530

    Initial commit

---
+++ app/src/main/java/com/waajid/nfcknife/NfcActivity.kt
@@ -0,0 +1,56 @@
+package com.waajid.nfcknife
+
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.IntentFilter
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+private var nfcAdapter: NfcAdapter? = null
+
+class NfcActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        enableForegroundDispatch()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        disableForegroundDispatch()
+    }
+
+    private fun enableForegroundDispatch() {
+        val intent = Intent(this, javaClass).apply {
+            addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        }
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        val filters = arrayOf(IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED), IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED))
+        nfcAdapter?.enableForegroundDispatch(this, pendingIntent, filters, null)
+    }
+
+    private fun disableForegroundDispatch() {
+        nfcAdapter?.disableForegroundDispatch(this)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        intent?.let {
+            if (it.action == NfcAdapter.ACTION_TECH_DISCOVERED) {
+                val tag: Tag? = it.getParcelableExtra(NfcAdapter.EXTRA_TAG)
+                tag?.let {
+                    Toast.makeText(this, "NFC Tag Detected!\nTag ID: ${tag.id}", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+}
